fa3093f1058f321011aa90b816cc138457add2d2
Anurag <coleridgeprotube@gmail.com>
2024-09-15 23:46:05 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home(){
+    return <main className={`flex flex-col items-center justify-center m-5`}>
+    <h1 className={`text-3xl`}>Welcome to agrodeal!</h1>
+    </main>
+}
